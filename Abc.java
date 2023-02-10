440f452c026238d84d7e6a6c3e4674f2b66c1a77
Sonaa626 <123633882+Sonaa626@users.noreply.github.com>
2023-02-10 18:26:05 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -7,4 +7,14 @@ class Abc
     int c=a+b;
     System.out.print(c);
   }
+  static void add()
+  {
+    int a=10;
+    int b=20;
+    int c=a+b;
+    System.out.print(c);
+  }
+  public static void main(String[] args)
+  {
+    add();
 }
